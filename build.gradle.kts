6e3ee60fd96aa86e004b18d8ce4bd510b8ca799c
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2018-04-05 13:07:00 -0300

    Configure JaCoCo tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,31 @@
 import org.gradle.kotlin.dsl.kotlin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+fun DependencyHandler.junit(module: String) = "org.junit.jupiter:junit-jupiter-$module:5.1.0"
+
 plugins {
-    base
-    kotlin("jvm") version "1.2.31" apply false
+    kotlin("jvm") version "1.2.31"
 }
 
-allprojects {
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("jacoco")
+    }
+
     group = "org.valiktor"
     version = "0.1.0"
 
     repositories {
         mavenCentral()
+    }
+
+    dependencies {
+        compile(kotlin("stdlib-jdk8"))
+        compile(kotlin("reflect"))
+
+        testImplementation(junit("api"))
+        testRuntimeOnly(junit("engine"))
     }
 
     tasks {
@@ -23,6 +37,28 @@ allprojects {
 
         withType<Test> {
             useJUnitPlatform()
+        }
+
+        withType<JacocoReport> {
+            reports {
+                xml.isEnabled = true
+                html.isEnabled = true
+            }
+
+            val jacocoTestCoverageVerification by tasks
+            jacocoTestCoverageVerification.dependsOn(this)
+        }
+
+        withType<JacocoCoverageVerification> {
+            violationRules {
+                rule {
+                    limit {
+                        minimum = BigDecimal.valueOf(1.0)
+                    }
+                }
+            }
+            val check by tasks
+            check.dependsOn(this)
         }
     }
 }
