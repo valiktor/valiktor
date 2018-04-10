a0a6456ad607a84a8dd81d22a4972bab0c78bbcd
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2018-04-10 17:53:04 -0300

    Write tests for Set<I18nConstraintViolation> functions

--- valiktor-core/src/test/kotlin/org/valiktor/i18n/I18nConstraintViolationTest.kt
+++ valiktor-core/src/test/kotlin/org/valiktor/i18n/I18nConstraintViolationTest.kt
@@ -1,5 +1,6 @@
 package org.valiktor.i18n
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
@@ -14,6 +15,11 @@ private object I18nConstraintViolationFixture {
 
     fun createConstraintViolation(): ConstraintViolation =
             DefaultConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"))
+
+    fun createConstraintViolations(): Set<ConstraintViolation> = setOf(
+            DefaultConstraintViolation(property = "id", value = 1, constraint = NotEquals("1")),
+            DefaultConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test")),
+            DefaultConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com")))
 }
 
 class I18nConstraintViolationTest {
@@ -146,5 +152,118 @@ class I18nConstraintViolationTest {
                 { assertEquals(i18nConstraintViolation.constraint, NotEquals("Test")) },
                 { assertEquals(i18nConstraintViolation.message, "Should not be equal to Test") }
         )
+    }
+}
+
+class I18nConstraintViolationSetTest {
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with default params`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n()
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Must not be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Must not be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Must not be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom locale`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(locale = Locale("pt", "BR"))
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Não deve ser igual a 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Não deve ser igual a Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Não deve ser igual a test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom baseName`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(baseName = "testMessages")
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Should not be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Should not be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Should not be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom key`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(key = { "org.valiktor.constraints.Equals.message" })
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Must be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Must be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Must be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom locale and baseName`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(locale = ENGLISH, baseName = "testMessages")
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Cannot be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Cannot be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Cannot be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom locale and key`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(locale = ENGLISH, key = { "org.valiktor.constraints.Equals.message" })
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Must be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Must be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Must be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom baseName and key`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(baseName = "testMessages", key = { "org.valiktor.constraints.NotEquals.message.custom" })
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Cannot be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Cannot be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Cannot be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom locale, baseName and key`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(locale = ENGLISH, baseName = "testMessages", key = { "org.valiktor.constraints.NotEquals.message.custom" })
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Should not be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Should not be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Should not be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom ResourceBundle`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(resourceBundle = getBundle("testMessages", Locale("")))
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Should not be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Should not be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Should not be equal to test@test.com"))
+    }
+
+    @Test
+    fun `should convert to set of I18nConstraintViolation with custom ResourceBundle and key`() {
+        val i18nConstraintViolations = I18nConstraintViolationFixture.createConstraintViolations()
+                .mapToI18n(resourceBundle = getBundle("testMessages", ENGLISH), key = { "org.valiktor.constraints.NotEquals.message.custom" })
+
+        assertThat(i18nConstraintViolations).containsExactly(
+                DefaultI18nConstraintViolation(property = "id", value = 1, constraint = NotEquals("1"), message = "Should not be equal to 1"),
+                DefaultI18nConstraintViolation(property = "name", value = "Test", constraint = NotEquals("Test"), message = "Should not be equal to Test"),
+                DefaultI18nConstraintViolation(property = "email", value = "test@test.com", constraint = NotEquals("test@test.com"), message = "Should not be equal to test@test.com"))
     }
 }
