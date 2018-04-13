4a083626e4eb5dc9b8ba269bd581d9c14261e528
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2018-04-13 09:56:49 -0300

    Improve test scenarios

--- valiktor-core/src/test/kotlin/org/valiktor/ValidatorTest.kt
+++ valiktor-core/src/test/kotlin/org/valiktor/ValidatorTest.kt
@@ -41,11 +41,14 @@ private object ValidatorFixture {
 class ValidatorTest {
 
     @Test
-    fun `should validate isNull`() {
+    fun `isNull with null property should be valid`() {
         Employee().validate {
             Employee::id.isNull()
         }
+    }
 
+    @Test
+    fun `isNull with not null property should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 1).validate {
                 Employee::id.isNull()
@@ -67,11 +70,14 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isNotNull`() {
+    fun `isNotNull with not null property should be valid`() {
         Employee(id = 1).validate {
             Employee::id.isNotNull()
         }
+    }
 
+    @Test
+    fun `isNotNull with null property should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee().validate {
                 Employee::id.isNotNull()
@@ -93,14 +99,21 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isEqualTo`() {
-        Employee(id = 1).validate {
-            Employee::id.isEqualTo(1)
-        }
+    fun `isEqualTo with null property should be valid`() {
         Employee().validate {
             Employee::id.isEqualTo(1)
         }
+    }
 
+    @Test
+    fun `isEqualTo with same value should be valid`() {
+        Employee(id = 1).validate {
+            Employee::id.isEqualTo(1)
+        }
+    }
+
+    @Test
+    fun `isEqualTo with different value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 2).validate {
                 Employee::id.isEqualTo(1)
@@ -122,14 +135,21 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isNotEqualTo`() {
-        Employee(id = 2).validate {
-            Employee::id.isNotEqualTo(1)
-        }
+    fun `isNotEqualTo with null property should be valid`() {
         Employee().validate {
             Employee::id.isNotEqualTo(1)
         }
+    }
 
+    @Test
+    fun `isNotEqualTo with different value should be valid`() {
+        Employee(id = 2).validate {
+            Employee::id.isNotEqualTo(1)
+        }
+    }
+
+    @Test
+    fun `isNotEqualTo with same value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 1).validate {
                 Employee::id.isNotEqualTo(1)
@@ -151,14 +171,21 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isIn with vararg`() {
-        Employee(id = 2).validate {
-            Employee::id.isIn(1, 2, 3)
-        }
+    fun `isIn vararg with null property should be valid`() {
         Employee().validate {
             Employee::id.isIn(1, 2, 3)
         }
+    }
 
+    @Test
+    fun `isIn vararg with same value should be valid`() {
+        Employee(id = 2).validate {
+            Employee::id.isIn(1, 2, 3)
+        }
+    }
+
+    @Test
+    fun `isIn vararg with different value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 1).validate {
                 Employee::id.isIn(0, 2, 3)
@@ -180,14 +207,21 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isIn with iterable`() {
-        Employee(id = 2).validate {
-            Employee::id.isIn(listOf(1, 2, 3))
-        }
+    fun `isIn iterable with null property should be valid`() {
         Employee().validate {
             Employee::id.isIn(listOf(1, 2, 3))
         }
+    }
 
+    @Test
+    fun `isIn iterable with same value should be valid`() {
+        Employee(id = 2).validate {
+            Employee::id.isIn(listOf(1, 2, 3))
+        }
+    }
+
+    @Test
+    fun `isIn iterable with different value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 1).validate {
                 Employee::id.isIn(listOf(0, 2, 3))
@@ -209,14 +243,21 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isNotIn with vararg`() {
-        Employee(id = 1).validate {
-            Employee::id.isNotIn(0, 2, 3)
-        }
+    fun `isNotIn vararg with null property should be valid`() {
         Employee().validate {
             Employee::id.isNotIn(0, 2, 3)
         }
+    }
 
+    @Test
+    fun `isNotIn vararg with different value should be valid`() {
+        Employee(id = 1).validate {
+            Employee::id.isNotIn(0, 2, 3)
+        }
+    }
+
+    @Test
+    fun `isNotIn vararg with same value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 1).validate {
                 Employee::id.isNotIn(1, 2, 3)
@@ -238,14 +279,21 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isNotIn with iterable`() {
-        Employee(id = 1).validate {
-            Employee::id.isNotIn(listOf(0, 2, 3))
-        }
+    fun `isNotIn iterable with null property should be valid`() {
         Employee().validate {
             Employee::id.isNotIn(listOf(0, 2, 3))
         }
+    }
 
+    @Test
+    fun `isNotIn iterable with different value should be valid`() {
+        Employee(id = 1).validate {
+            Employee::id.isNotIn(listOf(0, 2, 3))
+        }
+    }
+
+    @Test
+    fun `isNotIn iterable with same value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
             Employee(id = 1).validate {
                 Employee::id.isNotIn(listOf(1, 2, 3))
@@ -267,39 +315,44 @@ class ValidatorTest {
     }
 
     @Test
-    fun `should validate isValid`() {
+    fun `isValid with null property should be valid`() {
         Employee().validate {
-            Employee::id.isValid { it == null }
+            Employee::id.isValid { it == 1 }
         }
+    }
 
+    @Test
+    fun `isValid with same value should be valid`() {
+        Employee(id = 1).validate {
+            Employee::id.isValid { it == 1 }
+        }
+    }
+
+    @Test
+    fun `isValid with different value should be invalid`() {
         val exception = assertThrows<ConstraintViolationException> {
-            Employee(id = 1).validate {
-                Employee::id.isValid { it == null }
+            Employee(id = 2).validate {
+                Employee::id.isValid { it == 1 }
             }
         }
         assertThat(exception.constraintViolations).containsExactly(
-                DefaultConstraintViolation(property = "id", value = 1, constraint = Valid<Int?>({ it == null })))
+                DefaultConstraintViolation(property = "id", value = 2, constraint = Valid<Int?>({ it == 1 })))
 
         val i18nMap: Map<Locale, Set<I18nConstraintViolation>> = ValidatorFixture.supportedLocales
                 .map { it to exception.constraintViolations.mapToI18n(it) }.toMap()
 
         assertThat(i18nMap).containsExactly(
                 entry(Locales.DEFAULT, setOf(DefaultI18nConstraintViolation(
-                        property = "id", value = 1, constraint = Valid<Int?>({ it == null }), message = "Must be valid"))),
+                        property = "id", value = 2, constraint = Valid<Int?>({ it == 1 }), message = "Must be valid"))),
                 entry(Locales.EN, setOf(DefaultI18nConstraintViolation(
-                        property = "id", value = 1, constraint = Valid<Int?>({ it == null }), message = "Must be valid"))),
+                        property = "id", value = 2, constraint = Valid<Int?>({ it == 1 }), message = "Must be valid"))),
                 entry(Locales.PT_BR, setOf(DefaultI18nConstraintViolation(
-                        property = "id", value = 1, constraint = Valid<Int?>({ it == null }), message = "Deve ser válido"))))
+                        property = "id", value = 2, constraint = Valid<Int?>({ it == 1 }), message = "Deve ser válido"))))
     }
 
     @Test
-    fun `should validate sub objects`() {
-        val validEmployee = Employee(id = 1,
-                company = Company(id = 1),
-                address = Address(id = 1, city = City(id = 1, state = State(id = 1, country = Country(id = 1)))))
-
-        validEmployee.validate {
-            Employee::id.isNotNull()
+    fun `inner null properties should be valid`() {
+        Employee().validate {
             Employee::company.validate {
                 Company::id.isNotNull()
             }
@@ -316,30 +369,57 @@ class ValidatorTest {
                 }
             }
         }
+    }
 
-        val exception = assertThrows<ConstraintViolationException> {
-            val invalidEmployee = Employee(
-                    company = Company(),
-                    address = Address(city = City(state = State(country = Country()))))
-
-            invalidEmployee.validate {
-                Employee::id.isNotNull()
-                Employee::company.validate {
-                    Company::id.isNotNull()
-                }
-                Employee::address.validate {
-                    Address::id.isNotNull()
-                    Address::city.validate {
-                        City::id.isNotNull()
-                        City::state.validate {
-                            State::id.isNotNull()
-                            State::country.validate {
-                                Country::id.isNotNull()
+    @Test
+    fun `inner not null properties should be valid`() {
+        Employee(id = 1, company = Company(id = 1), address =
+        Address(id = 1, city = City(id = 1, state =
+        State(id = 1, country = Country(id = 1)))))
+                .validate {
+                    Employee::id.isNotNull()
+                    Employee::company.validate {
+                        Company::id.isNotNull()
+                    }
+                    Employee::address.validate {
+                        Address::id.isNotNull()
+                        Address::city.validate {
+                            City::id.isNotNull()
+                            City::state.validate {
+                                State::id.isNotNull()
+                                State::country.validate {
+                                    Country::id.isNotNull()
+                                }
                             }
                         }
                     }
                 }
-            }
+    }
+
+    @Test
+    fun `inner null properties should be invalid`() {
+        val exception = assertThrows<ConstraintViolationException> {
+            Employee(company = Company(), address =
+            Address(city = City(state =
+            State(country = Country()))))
+                    .validate {
+                        Employee::id.isNotNull()
+                        Employee::company.validate {
+                            Company::id.isNotNull()
+                        }
+                        Employee::address.validate {
+                            Address::id.isNotNull()
+                            Address::city.validate {
+                                City::id.isNotNull()
+                                City::state.validate {
+                                    State::id.isNotNull()
+                                    State::country.validate {
+                                        Country::id.isNotNull()
+                                    }
+                                }
+                            }
+                        }
+                    }
         }
 
         assertThat(exception.constraintViolations).containsExactly(
@@ -375,5 +455,33 @@ class ValidatorTest {
                         DefaultI18nConstraintViolation(property = "address.city.id", constraint = NotNull(), message = "Não deve ser nulo"),
                         DefaultI18nConstraintViolation(property = "address.city.state.id", constraint = NotNull(), message = "Não deve ser nulo"),
                         DefaultI18nConstraintViolation(property = "address.city.state.country.id", constraint = NotNull(), message = "Não deve ser nulo"))))
+    }
+
+    @Test
+    fun `should not repeat the property`() {
+        val exception = assertThrows<ConstraintViolationException> {
+            Employee().validate {
+                Employee::id.isNotNull().isEqualTo(1).isIn(1, 2, 3)
+                Employee::name.isNotNull().isEqualTo("test").isIn("test1", "test2", "test3")
+            }
+        }
+
+        assertThat(exception.constraintViolations).containsExactly(
+                DefaultConstraintViolation(property = "id", constraint = NotNull()),
+                DefaultConstraintViolation(property = "name", constraint = NotNull()))
+
+        val i18nMap: Map<Locale, Set<I18nConstraintViolation>> = ValidatorFixture.supportedLocales
+                .map { it to exception.constraintViolations.mapToI18n(it) }.toMap()
+
+        assertThat(i18nMap).containsExactly(
+                entry(Locales.DEFAULT, setOf(
+                        DefaultI18nConstraintViolation(property = "id", constraint = NotNull(), message = "Must not be null"),
+                        DefaultI18nConstraintViolation(property = "name", constraint = NotNull(), message = "Must not be null"))),
+                entry(Locales.EN, setOf(
+                        DefaultI18nConstraintViolation(property = "id", constraint = NotNull(), message = "Must not be null"),
+                        DefaultI18nConstraintViolation(property = "name", constraint = NotNull(), message = "Must not be null"))),
+                entry(Locales.PT_BR, setOf(
+                        DefaultI18nConstraintViolation(property = "id", constraint = NotNull(), message = "Não deve ser nulo"),
+                        DefaultI18nConstraintViolation(property = "name", constraint = NotNull(), message = "Não deve ser nulo"))))
     }
 }
