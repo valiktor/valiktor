348f069e21a41d051ce83c24ea8a5e78b86915ca
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-05-20 11:59:19 -0300

    Add valiktor-sample-collections

---
+++ valiktor-samples/valiktor-sample-collections/src/test/kotlin/org/valiktor/sample/SampleApplicationTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018-2019 https://www.valiktor.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.valiktor.sample
+
+import org.assertj.core.api.Assertions.assertThat
+import org.valiktor.ConstraintViolationException
+import org.valiktor.DefaultConstraintViolation
+import org.valiktor.constraints.NotBlank
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class SampleApplicationTest {
+
+    @Test
+    fun `should validate employee`() {
+        val exception = assertFailsWith<ConstraintViolationException> {
+            Employee(
+                dependents = listOf(
+                    Dependent(name = ""),
+                    Dependent(name = " "),
+                    Dependent(name = "  ")
+                )
+            )
+        }
+
+        assertThat(exception.constraintViolations).containsExactly(
+            DefaultConstraintViolation(property = "dependents[0].name", value = "", constraint = NotBlank),
+            DefaultConstraintViolation(property = "dependents[1].name", value = " ", constraint = NotBlank),
+            DefaultConstraintViolation(property = "dependents[2].name", value = "  ", constraint = NotBlank)
+        )
+    }
+}
