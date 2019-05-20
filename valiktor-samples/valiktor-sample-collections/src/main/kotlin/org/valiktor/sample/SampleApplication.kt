309e89e70afeec1049ba838ff63435d32902577f
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-05-20 11:59:19 -0300

    Add valiktor-sample-collections

---
+++ valiktor-samples/valiktor-sample-collections/src/main/kotlin/org/valiktor/sample/SampleApplication.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018-2019 https://www.valiktor.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.valiktor.sample
+
+import org.valiktor.ConstraintViolationException
+import org.valiktor.functions.isNotBlank
+import org.valiktor.functions.validateForEach
+import org.valiktor.i18n.toMessage
+import org.valiktor.validate
+import java.util.Locale
+
+data class Dependent(val name: String)
+
+data class Employee(val dependents: List<Dependent>) {
+    init {
+        validate(this) {
+            validate(Employee::dependents).validateForEach {
+                validate(Dependent::name).isNotBlank()
+            }
+        }
+    }
+}
+
+fun main() {
+    try {
+        Employee(
+            dependents = listOf(
+                Dependent(name = ""),
+                Dependent(name = " "),
+                Dependent(name = "  ")
+            )
+        )
+    } catch (ex: ConstraintViolationException) {
+        ex.constraintViolations
+            .map { it.toMessage(locale = Locale.ENGLISH) }
+            .map { "${it.property}: ${it.message}" }
+            .forEach(::println)
+    }
+}
