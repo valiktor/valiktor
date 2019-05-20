8bc34ba93fbcfe236ed5f42121f48141305a5e07
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-05-20 11:03:48 -0300

    Add valiktor-sample-javatime

---
+++ valiktor-samples/valiktor-sample-javatime/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    compile(kotlin("stdlib"))
+    compile(kotlin("reflect"))
+    compile(project(":valiktor-core"))
+    compile(project(":valiktor-javatime"))
+}
--- valiktor-samples/valiktor-sample-javatime/src/test/kotlin/org/valiktor/sample/SampleApplicationTest.kt
+++ valiktor-samples/valiktor-sample-javatime/src/test/kotlin/org/valiktor/sample/SampleApplicationTest.kt
@@ -19,10 +19,10 @@ package org.valiktor.sample
 import org.assertj.core.api.Assertions.assertThat
 import org.valiktor.ConstraintViolationException
 import org.valiktor.DefaultConstraintViolation
-import org.valiktor.constraints.DecimalDigits
-import org.valiktor.constraints.Email
-import org.valiktor.constraints.Greater
-import org.valiktor.constraints.Size
+import org.valiktor.constraints.Between
+import org.valiktor.constraints.LessOrEqual
+import java.time.LocalDate
+import java.time.LocalTime
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
 
@@ -32,18 +32,28 @@ class SampleApplicationTest {
     fun `should validate employee`() {
         val exception = assertFailsWith<ConstraintViolationException> {
             Employee(
-                id = -1,
-                name = "aa",
-                email = "aaa",
-                salary = 9999.999
+                dateOfBirth = LocalDate.now(),
+                workStartTime = LocalTime.of(7, 0, 0),
+                workEndTime = LocalTime.of(21, 0, 0)
             )
         }
 
         assertThat(exception.constraintViolations).containsExactly(
-            DefaultConstraintViolation(property = "id", value = -1, constraint = Greater(0)),
-            DefaultConstraintViolation(property = "name", value = "aa", constraint = Size(min = 3, max = 30)),
-            DefaultConstraintViolation(property = "email", value = "aaa", constraint = Email),
-            DefaultConstraintViolation(property = "salary", value = 9999.999, constraint = DecimalDigits(max = 2))
+            DefaultConstraintViolation(
+                property = "dateOfBirth",
+                value = LocalDate.now(),
+                constraint = LessOrEqual(LocalDate.now().minusYears(18L))
+            ),
+            DefaultConstraintViolation(
+                property = "workStartTime",
+                value = LocalTime.of(7, 0, 0),
+                constraint = Between(start = LocalTime.of(8, 0, 0), end = LocalTime.of(10, 0, 0))
+            ),
+            DefaultConstraintViolation(
+                property = "workEndTime",
+                value = LocalTime.of(21, 0, 0),
+                constraint = Between(start = LocalTime.of(18, 0, 0), end = LocalTime.of(20, 0, 0))
+            )
         )
     }
 }
