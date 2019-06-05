ca8b666dd2f7ae2a4633401ef57a2d918f31172d
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2019-06-05 10:49:04 -0300

    Add valiktor-sample-jodamoney

---
+++ valiktor-samples/valiktor-sample-jodamoney/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    compile(kotlin("stdlib"))
+    compile(kotlin("reflect"))
+    compile(project(":valiktor-core"))
+    compile(project(":valiktor-jodamoney"))
+    compile("org.joda:joda-money:1.0.1")
+}
