76e8314cdac244755bc68491b7b6c350e62fb259
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2020-03-29 21:11:44 -0300

    Updates license copyright

--- valiktor-javamoney/src/test/kotlin/org/valiktor/i18n/JavaMoneyFormatterSpiTest.kt
+++ valiktor-javamoney/src/test/kotlin/org/valiktor/i18n/JavaMoneyFormatterSpiTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 https://www.valiktor.org
+ * Copyright 2018-2020 https://www.valiktor.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
